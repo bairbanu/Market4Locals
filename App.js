88c9c07ea500f2537d2c16fea147174de9659e9f
zubairnahmed <zubair.ahmed@alumni.stanford.edu>
2018-02-03 15:37:34 -0800

    connect Web3 with React Native and test connection with Ethereum Mainnet by logging latest block

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
+import './global';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+const Web3 = require('web3');
 
 export default class App extends React.Component {
+  componentWillMount() {
+    const web3 = new Web3(
+      new Web3.providers.HttpProvider('https://mainnet.infura.io/')
+    );
+
+    web3.eth.getBlock('latest').then(console.log)
+  }
+
   render() {
     return (
       <View style={styles.container}>
