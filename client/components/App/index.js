62cd6ed05ad937d73cf136a1abd748ad69abf79e
zubairnahmed <zubair.ahmed@alumni.stanford.edu>
2018-02-03 21:53:49 -0800

    integrate web3.js and debug ReferenceError in navigation

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -7,15 +7,26 @@ import { StyleSheet } from 'react-native';
 const Web3 = require('web3');
 
 export default class App extends React.Component {
-  componentWillMount() {
+  constructor(props) {
+    super(props);
+
     const web3 = new Web3(
       new Web3.providers.HttpProvider('https://ropsten.infura.io/')
     );
 
-    web3.eth.getBlock('latest').then(console.log)
+    const ABI = [ { "constant": true, "inputs": [], "name": "paymentAccepted", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "pure", "type": "function" }, { "constant": true, "inputs": [], "name": "paymentDisbursed", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "pure", "type": "function" } ];
+    const smartContractAddress = '0x27fc9269b9b266b3af06c98ca7f69e5872002fa3';
+
+    const ContractInstance = new web3.eth.Contract(ABI);
+    ContractInstance.options.address = smartContractAddress;
+
+    this.state = {
+      ContractInstance
+    }
   }
 
   render() {
+    this.state.ContractInstance.methods.paymentAccepted().call().then(console.log)
     return (
       <Provider store={createStore(reducers)}>
         <Tabs style={styles.navbar} />
