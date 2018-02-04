d15999b94079534bf3a2f72643d5eecd0cad5cc4
pkallas <pkallas@Patricks-MacBook-Pro.local>
2018-02-04 10:17:07 -0800

    Added a loading component and actions, fixed binding issues

--- client/reducers/marketListReducer.js
+++ client/reducers/marketListReducer.js
@@ -5,39 +5,59 @@ import {
   LOGIN_FAILURE,
   SIGNUP_SUCCESS,
   SIGNUP_FAILURE,
+  LOADING,
+  DONE_LOADING
 } from '../actions';
 
-export default function(state = [], action) {
+export default function(state = { isLoggedIn: true }, action) {
   switch (action.type) {
     case FETCH_ITEMS_SUCCESS:
       return {
         items: action.payload,
         isLoggedIn: state.isLoggedIn,
+        loading: state.loading,
       };
     case FETCH_ITEMS_FAILURE:
       return {
         error: action.payload,
         isLoggedIn: state.isLoggedIn,
+        loading: state.loading,
       };
     case LOGIN_SUCCESS:
       return {
         items: state.items,
         isLoggedIn: action.payload,
+        loading: state.loading,
       };
     case LOGIN_FAILURE:
       return {
         items: state.items,
         isLoggedIn: action.payload,
+        loading: state.loading,
       };
     case SIGNUP_SUCCESS:
       return {
         items: state.items,
         isLoggedIn: action.payload,
+        loading: state.loading,
       };
     case SIGNUP_FAILURE:
       return {
         items: state.items,
         isLoggedIn: action.payload,
+        loading: state.loading,
+      };
+    case LOADING:
+      return {
+        items: state.items,
+        isLoggedIn: state.isLoggedIn,
+        loading: action.loading,
+      };
+    case DONE_LOADING:
+      return {
+        items: state.items,
+        isLoggedIn: state.isLoggedIn,
+        loading: action.loading,
       };
     default:
       return state;
