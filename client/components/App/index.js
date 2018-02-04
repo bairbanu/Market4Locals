598818942f3c6160321c3089caa1241f76a1f386
pkallas <pkallas@Patricks-MacBook-Pro.local>
2018-02-03 21:21:03 -0800

    Added navigation bar

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../../reducers/index.js';
-import MarketList from '../../containers/MarketList/index.js';
+import { Tabs } from '../Navigation/index.js';
+import { StyleSheet } from 'react-native';
 const Web3 = require('web3');
 
 export default class App extends React.Component {
@@ -17,8 +18,14 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <MarketList />
+        <Tabs style={styles.navbar} />
       </Provider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  navbar: {
+    marginTop: 30,
+  },
+});
