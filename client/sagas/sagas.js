7cecb23fe8c61b096f8ae941ffa4c6f1817ed8e1
zubairnahmed <zubair.ahmed@alumni.stanford.edu>
2018-02-04 10:23:00 -0800

    sage provides development data

--- client/sagas/sagas.js
+++ client/sagas/sagas.js
@@ -1,27 +1,72 @@
 import { takeLatest, call, put } from 'redux-saga/effects';
-import axios from 'axios';
 import { FETCH_ITEMS_REQUEST, FETCH_ITEMS_FAILURE, FETCH_ITEMS_SUCCESS}  from '../actions/types';
 
 function getFromDatabase(url) {
-  axios.get(url)
-  .then(response => {
-    console.log('response:::', response)
-    return response.data
-  })
-  .then((test) => {
-    return test;
-  })
-  .catch(error => {
-    console.error('An error occured:', error)
-    throw error.message;
-  })
+  return [
+    {
+      "title": "Selling my cat",
+      "img_url": "https://www.petmd.com/sites/default/files/petmd-cat-happy-10.jpg",
+      "description": "Out of money. Must sell cat",
+      "price": "$1500",
+      "isBought": false,
+      "isDelivered": false,
+      "inDelivery": false,
+    },
+    {
+      "title": "Selling another cat",
+      "img_url": "https://www.petmd.com/sites/default/files/petmd-cat-happy-10.jpg",
+      "description": "Still out of money. No good. Close to eating my cat. Save it before that happens.",
+      "price": "$2500",
+      "isBought": true,
+      "isDelivered": false,
+      "inDelivery": false,
+    },
+    {
+      "title": "Selling YET another cat",
+      "img_url": "https://www.petmd.com/sites/default/files/petmd-cat-happy-10.jpg",
+      "description": "I ate the last one. New cat. Fresh.",
+      "price": "$500",
+      "isBought": true,
+      "isDelivered": false,
+      "inDelivery": true,
+    },
+    {
+      "title": "Selling YET YET another cat",
+      "img_url": "https://www.petmd.com/sites/default/files/petmd-cat-happy-10.jpg",
+      "description": "SO TASTY!",
+      "price": "$500",
+      "isBought": true,
+      "isDelivered": true,
+      "inDelivery": true,
+    },
+    {
+      "title": "Selling ANOTHER cat",
+      "img_url": "https://www.petmd.com/sites/default/files/petmd-cat-happy-10.jpg",
+      "description": "I actually love this one very much.",
+      "price": "$500",
+      "isBought": false,
+      "isDelivered": false,
+      "inDelivery": false,
+    },
+    {
+      "title": "Vanilla cat",
+      "img_url": "https://www.petmd.com/sites/default/files/petmd-cat-happy-10.jpg",
+      "description": "Don't hate on simplicity.",
+      "price": "$500",
+      "isBought": false,
+      "isDelivered": false,
+      "inDelivery": false,
+    },
+  ]
 }
 
 function* fetchItems(action) {
   try {
     const items = yield call(getFromDatabase, action.payload);
+    console.log('these are items:::', items)
     yield put({ type: FETCH_ITEMS_SUCCESS, payload: items });
   } catch (e) {
+    console.log('ERROR message::::::::::::', e)
     yield put({ type: FETCH_ITEMS_FAILURE, error: e.message })
   }
 }
